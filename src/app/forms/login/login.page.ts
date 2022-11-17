16920467cf5887fb84b56049a61110e1cc344d6f
vitoriataborda <vitorianahitab@gmail.com>
2022-11-17 15:02:36 -0300

    vali

--- src/app/forms/login/login.page.ts
+++ src/app/forms/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  formLogin = this.formBuilder.group({nomeEmail:'',});
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
   }
